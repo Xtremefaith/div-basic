070560b36c972a96df910096a7ea85c69ce90291
Nick Worth <nick@divtruth.com>
2014-02-23 06:27:09 -0700

    clean up index basic loop template, remove bones pagination

--- index.php
+++ index.php
@@ -6,73 +6,55 @@
 			
 				    <div id="main" class="eightcol first clearfix" role="main">
 						
-						<?php $pagename = get_query_var('pagename'); ?>						
-						<h1 class="page-title title-margin" itemprop="headline"><?php echo $pagename; ?></h1>
-
-						<?php $counter = 0; ?>
+						<?php # THE LOOP ?>
 					    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>						
-						<?php $counter++; ?>
 					
-					    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-							<?php if ($counter > 1): ?>
-						    	<?php if (has_post_thumbnail()): ?>
-									<div class="entry-content"><?php the_post_thumbnail( 'blog' ); ?></div>
-								<?php endif; ?>
-							<?php endif; ?>
+					    	<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-						    <header class="article-header">
+						    	<header class="article-header">
 					
-							    <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-								<p class="byline vcard"><?php
-									printf(__('<time class="updated" datetime="%1$s" pubdate>%2$s @ %3$s </time>', 'divtruth'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_time('g:iA'));
-									printf(__('<span class="tags">%1$s</span', 'divtruth'), get_the_category_list(', '));
-								?></p>
+							    	<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+									<p class="byline vcard"><?php
+										printf(__('<time class="updated" datetime="%1$s" pubdate>%2$s @ %3$s </time>', 'divtruth'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_time('g:iA'));
+										printf(__('<span class="tags">%1$s</span', 'divtruth'), get_the_category_list(', '));
+									?></p>
 						
-						    </header> <!-- end article header -->
+							    </header> <!-- end article header -->
 					
-						    <section class="entry-content clearfix">
-								<?php if ($counter > 1): ?>
-									<?php if (has_post_thumbnail()): ?>
-										<div class="pad-left-200"><?php the_excerpt(); ?></div>
-									<?php else : ?>
-										<?php the_excerpt(); ?>
+							    <section class="entry-content clearfix">
+									<?php if ($counter > 1): ?>
+										<?php if (has_post_thumbnail()): ?>
+											<div class="pad-left-200"><?php the_excerpt(); ?></div>
+										<?php else : ?>
+											<?php the_excerpt(); ?>
+										<?php endif; ?>
+									<?php elseif ($counter == 1): ?>
+								    	<?php the_content(); ?>
+								    	<hr>
 									<?php endif; ?>
-								<?php elseif ($counter == 1): ?>
-							    	<?php the_content(); ?>
-							    	<hr>
-								<?php endif; ?>
-						    </section> <!-- end article section -->
+							    </section> <!-- end article section -->
 						
-						    <footer class="article-footer">
-    							<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'bonestheme') . '</span> ', ', ', ''); ?></p>
+							    <footer class="article-footer">
+	    							<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'divtruth') . '</span> ', ', ', ''); ?></p>
 
-						    </footer> <!-- end article footer -->
+							    </footer> <!-- end article footer -->
 						    
-						    <?php // comments_template(); // uncomment if you want to use them ?>
-					
 					    </article> <!-- end article -->
 					
 					    <?php endwhile; ?>	
 					
-					        <?php if (function_exists('bones_page_navi')) { ?>
-					            <?php bones_page_navi(); ?>
-					        <?php } else { ?>
-					            <nav class="wp-prev-next">
-					                <ul class="clearfix">
-					        	        <li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "bonestheme")) ?></li>
-					        	        <li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "bonestheme")) ?></li>
-					                </ul>
-					            </nav>
-					        <?php } ?>		
+					        <div class="clearfix" style="width:100%;">
+						    	<?php div_pagination(); ?>
+						    </div>
 					
 					    <?php else : ?>
 					    
 					        <article id="post-not-found" class="hentry clearfix">
 					            <header class="article-header">
-					        	    <h1><?php _e("Post Not Found", "bonestheme"); ?></h1>
+					        	    <h1><?php _e("Post Not Found", "divtruth"); ?></h1>
 					        	</header>
 					            <section class="entry-content">
-					        	    <p><?php _e("Please visit our homepage.", "bonestheme"); ?></p>
+					        	    <p><?php _e("Please visit our homepage.", "divtruth"); ?></p>
  									<ul>
 										<li><a href="/">Home</a></li>
 									</ul>
